09b7962986d370d6b37212eb36f4961418ce62a5
Yessuremaybe <yn47579@gmail.com>
2026-03-26 22:54:14 -0700

    Mobile Menu!?
    WHoaAA :O !!!!!!!!!!!!!!!!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// ALL YOU JS GOES HERE
+const menuBtn = document.getElementById('menu-btn');
+const navMenu = document.getElementById('nav-menu');
+
+menuBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+  navMenu.classList.toggle('hidden');
+});
+
+navMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+  navMenu.classList.toggle('hidden');
+});
+
+
